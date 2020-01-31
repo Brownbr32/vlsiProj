4e1671032791f4e627e49115807381f9b96e71ce
bbrown <bbrown@rhel-cadence.ad.uvu.edu>
2020-01-31 15:08:33 -0700

    first commit

---
+++ .bash_history
@@ -0,0 +1,11 @@
+chsh tcsh
+chsh /bin/tcsh
+ls /bin
+ls /bin/t*
+ls /bin/tcsh
+chsh /bin/tcsh
+ls
+pwd
+man chsh
+chsh -s /bin/tcsh
+exit
---
+++ IC_CAD/CADENCE/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/tcsh
+setenv LOCAL_CADSETUP /usr/local/cad_common/local/setups/GPDK/gpdk045_v_6_0;
+set path = ($path /usr/local/cad_common/local/setups);
+cad-uvu &
